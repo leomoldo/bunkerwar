7eded8351ce769fc0f738a75761c416fb59d8fe4
leomoldo <leo.molczadzki@gmail.com>
2016-06-20 18:56:04 +0200

    Made GameSequencer Parcelable.

--- app/src/main/java/fr/leomoldo/android/bunkerwar/GameSequencer.java
+++ app/src/main/java/fr/leomoldo/android/bunkerwar/GameSequencer.java
@@ -1,27 +1,77 @@
 package fr.leomoldo.android.bunkerwar;
 
-public class GameSequencer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class GameSequencer implements Parcelable {
 
 	public enum GameState {
-		PLAYER_ONE_PLAYING,	
-		PLAYER_TWO_PLAYING,
+        PLAYER_ONE_PLAYING,
+        PLAYER_TWO_PLAYING,
 		PLAYER_ONE_FIRING,
 		PLAYER_TWO_FIRING,
 		PLAYER_ONE_WON,
-		PLAYER_TWO_WON
-	}
+        PLAYER_TWO_WON;
 
+        static GameState fromInt(int value) {
+            switch (value) {
+                case 0:
+                    return PLAYER_ONE_PLAYING;
+                case 1:
+                    return PLAYER_TWO_PLAYING;
+                case 2:
+                    return PLAYER_ONE_FIRING;
+                case 3:
+                    return PLAYER_TWO_FIRING;
+                case 4:
+                    return PLAYER_ONE_WON;
+                case 5:
+                    return PLAYER_TWO_WON;
+                default:
+                    return PLAYER_ONE_PLAYING;
+            }
+        }
+    }
 
     private Integer mRoundsCountPlayerOne;
     private Integer mRoundsCountPlayerTwo;
     private GameState mGameState;
-	
-	
-	public GameSequencer() {
+
+    public GameSequencer() {
         mRoundsCountPlayerOne = 1;
         mRoundsCountPlayerTwo = 0;
         mGameState = GameState.PLAYER_ONE_PLAYING;
-	}
+    }
+
+    protected GameSequencer(Parcel in) {
+        mRoundsCountPlayerOne = in.readInt();
+        mRoundsCountPlayerTwo = in.readInt();
+        mGameState = GameState.fromInt(in.readInt());
+    }
+
+    public static final Creator<GameSequencer> CREATOR = new Creator<GameSequencer>() {
+        @Override
+        public GameSequencer createFromParcel(Parcel in) {
+            return new GameSequencer(in);
+        }
+
+        @Override
+        public GameSequencer[] newArray(int size) {
+            return new GameSequencer[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mRoundsCountPlayerOne);
+        dest.writeInt(mRoundsCountPlayerTwo);
+        dest.writeInt(mGameState.ordinal());
+    }
 
     public Integer getRoundsCountPlayerOne() {
         return mRoundsCountPlayerOne;
